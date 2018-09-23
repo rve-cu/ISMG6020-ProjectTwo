c2ebf912bf46b53af205650c229ac1d36fb5218b
Rebekah Van Epps <rebekah.vanepps@ucdenver.edu>
2018-09-23 15:44:55 -0600

    mortgage calculator form: design, functionality; interest rate calculator form: design, functionality

--- NeptuneCalculator/frmMortgageCalculator.cs
+++ NeptuneCalculator/frmMortgageCalculator.cs
@@ -16,5 +16,41 @@ namespace NeptuneCalculator
         {
             InitializeComponent();
         }
+
+        private void btnCalculateMortgage_Click(object sender, EventArgs e)
+        {
+            // Convert input
+            int principalAmount = Convert.ToInt32(txtPrincipalAmount.Text);
+            int termLength = Convert.ToInt32(txtTermLength.Text);
+            double annualRate = Convert.ToDouble(txtAnnualRate.Text);
+
+            // Convert interest rate and term length to monthly amounts
+            double monthlyRate = annualRate / 12 / 100; // Divide annualRate by 12 (months in year) then divide by 100 to get decimal rate value
+            int termLengthMonths = termLength * 12;
+
+            // Calculate monthly payment
+            double monthlyPayment = (monthlyRate * principalAmount) / (1 - Math.Pow(1 + monthlyRate, -termLengthMonths));
+            // Calculate total amount owed
+            double totalOwed = monthlyPayment * termLengthMonths;
+            // Calculate interest paid
+            double totalInterest = totalOwed - principalAmount;
+
+            // Format and display calculated values
+            txtMonthlyPayment.Text = monthlyPayment.ToString("c");
+            txtTotalOwed.Text = totalOwed.ToString("c");
+            txtTotalInterest.Text = totalInterest.ToString("c");
+        }
+
+        private void btnExitMortgage_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnRate_Click(object sender, EventArgs e)
+        {
+            // Display Interest Rate Calculator form
+            frmRateCalculator form = new frmRateCalculator();
+            form.Show();
+        }
     }
 }
---
+++ NeptuneCalculator/frmRateCalculator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NeptuneCalculator
+{
+    public partial class frmRateCalculator : Form
+    {
+        public frmRateCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculateRate_Click(object sender, EventArgs e)
+        {
+            // Convert input
+            int creditScore = Convert.ToInt32(txtCreditScore.Text);
+
+            // Determine interest rate based on credit score
+            double interestRate = 0; // Initialize interest rate var
+            if (creditScore >= 750) // excellent
+            {
+                interestRate = 4.374;
+            }
+            else if (creditScore >=700 && creditScore < 750) // good
+            {
+                interestRate = 4.596;
+            }
+            else if (creditScore >= 650 && creditScore < 700) // fair
+            {
+                interestRate = 4.773;
+            }
+            else if (creditScore >= 600 && creditScore < 650) // poor
+            {
+                interestRate = 4.987;
+            }
+            else if (creditScore >= 550 && creditScore < 600) // bad
+            {
+                interestRate = 5.417;
+            }
+            else // terrible
+            {
+                interestRate = 5.963;
+            }
+
+            // Format and display calculated value
+            txtInterestRate.Text = interestRate.ToString("f3");
+        }
+
+        private void btnExitRate_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
